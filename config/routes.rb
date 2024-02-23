f5d31fef9a6c664b54399ceb1cf504c725864ef9
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-02-23 12:53:28 +0100

    heroku app

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :lists, only: [:index, :create, :show, :new] do
+  root to: "lists#index"
+  resources :lists, only: [:create, :show, :new] do
     resources :bookmarks, only: [:create, :new]
   end
   resources :bookmarks, only: [:destroy]
